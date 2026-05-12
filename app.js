e9218ec3f0336360a7f8d977b79b282fa606ad9b
sciencefact270-ux <sciencefact270@gmail.com>
2026-05-12 14:21:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-import { Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
-    }}>
-      <Text>Hello Brainrush 🚀</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Brainrush App 🚀</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
